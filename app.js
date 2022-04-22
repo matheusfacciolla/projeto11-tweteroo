d9237b74f30859813e7a629cdb1daed4541bf7ba
matheusfacciolla <matheustassifacciolla@gmail.com>
2022-04-22 10:20:19 -0300

    data validation and status code

--- app.js
+++ app.js
@@ -15,34 +15,45 @@ app.use(express.json());
 const users = [];
 const tweets = [];
 
-app.post('/sign-up', (req, res) => {
-    const { username, picture } = req.body;
+app.post("/sign-up", (req, res) => {
+    const { username, avatar } = req.body;
+    const userAlreadyExist = users.find((user) => user.username === username);
 
-    users.push({
-        username: username,
-        picture: picture
-    });
-
-    res.send(console.log("OK"));
+    if (username && avatar) {
+        if (userAlreadyExist) {
+            res.status(201).send("OK");
+        } else {
+            users.push({
+                username: username,
+                avatar: avatar
+            });
+            res.status(201).send("OK");
+        }
+    } else {
+        res.status(400).send("Todos os campos são obrigatórios!");
+    }
 });
 
-app.post('/tweets', (req, res) => {
-    const { username, tweet } = req.body;
+app.post("/tweets", (req, res) => {
+    const { username, tweet, avatar } = req.body;
 
-    tweets.push({
-        username: username,
-        tweet: tweet
-    });
-
-    res.send(console.log("OK"));
+    if (username && tweet) {
+        tweets.push({
+            username: username,
+            tweet: tweet,
+            avatar: avatar
+        });
+        res.status(201).send("OK");
+    } else {
+        res.status(400).send("Todos os campos são obrigatórios!");
+    }
 });
 
-app.get('/tweets', (req, res) => {
-    
-    tweets.forEach((tweet, index) => {
-        if(index >= tweets.length-10)
-        res.send(tweet);
-    })
+app.get("/tweets", (req, res) => {
+    const newTenTweets = tweets.slice(-10).reverse();
+    res.status(200).send(newTenTweets);
 });
 
-app.listen(5000, () => { console.log(chalk.bold.green(`Application working at http://localhost:5000`)) });
+app.listen(5000, () => {
+    console.log(chalk.bold.green(`Application working at http://localhost:5000`))
+});
